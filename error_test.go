a1088e3ce04bfd8f738c91f4587371c9e3f4546a
Kathurima <41376826+KathurimaKimathi@users.noreply.github.com>
2025-08-01 12:59:38 +0300

    fix: organization exist error method (#36)
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

---
+++ error_test.go
@@ -0,0 +1,30 @@
+package ocl
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestIsDuplicateMnemonicError(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want bool
+	}{
+		{
+			name: "Duplicate mnemonic error",
+			err: &APIError{
+				StatusCode: http.StatusBadRequest,
+				Mnemonic:   "already exists.",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsDuplicateMnemonicError(tt.err); got != tt.want {
+				t.Errorf("IsDuplicateMnemonicError() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
